fae13757e01a198a5e1bc84f27e4bdce2d79dab3
Axel Bagenal <axel.bagenal@giantmonkey.de>
2024-12-04 19:24:58 +0100

    day 1 part 1

---
+++ day_1.rb
@@ -0,0 +1,29 @@
+class PartOne
+  def initialize
+    @input = File.read('day_1_input.txt').split("\n")
+
+    @list1 = []
+    @list2 = []
+
+    @input.each do |line|
+      data = line.split.map(&:to_i)
+      @list1 << data[0]
+      @list2 << data[1]
+    end
+
+    # Sort both lists
+    @list1.sort!
+    @list2.sort!
+  end
+
+  def run
+    total_distance = 0
+    @list1.each_with_index do |item1, index|
+      item2 = @list2[index]
+      total_distance += (item1 - item2).abs
+    end
+    puts "part one: total distance: #{total_distance}"
+  end
+end
+
+PartOne.new.run
