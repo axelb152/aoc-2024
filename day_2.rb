f2ea25a935bee0521c2d859ddd1e6e48d8ba2f14
Axel Bagenal <axel.bagenal@giantmonkey.de>
2025-01-27 18:39:31 +0100

    day two part 1 with example data

--- day_2.rb
+++ day_2.rb
@@ -13,3 +13,41 @@
 #         Increment the safe reports count
 
 # Output the total count of safe reports
+
+
+require 'pry'
+class DayTwo
+  def initialize
+    @input = File.read('d2_example_input.txt').split("\n")
+
+    @safe_reports_count = 0
+  end
+
+  def part_one
+    @input.each do |line|
+      data = line.split.map(&:to_i)
+      safe = true
+
+      increasing = data.each_cons(2).all? { |a, b| a < b }
+      decreasing = data.each_cons(2).all? { |a, b| a > b }
+
+      if increasing || decreasing
+        data.each_cons(2) do |a, b|
+          diff = (a - b).abs
+          unless diff.between?(1, 3)
+            safe = false
+            break
+          end
+        end
+      else
+        safe = false
+      end
+
+      @safe_reports_count +=1 if safe
+    end
+
+    puts "Total count of safe reports is #{@safe_reports_count}"
+  end
+end
+
+DayTwo.new.part_one
