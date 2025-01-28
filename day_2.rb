45072a6c18d31983930b4bb311edd65a2241392b
Axel Bagenal <axel.bagenal@giantmonkey.de>
2025-01-28 10:11:05 +0100

    day 2 completed (cheated for part two)

--- day_2.rb
+++ day_2.rb
@@ -26,28 +26,59 @@ class DayTwo
   def part_one
     @input.each do |line|
       data = line.split.map(&:to_i)
-      safe = true
-
-      increasing = data.each_cons(2).all? { |a, b| a < b }
-      decreasing = data.each_cons(2).all? { |a, b| a > b }
-
-      if increasing || decreasing
-        data.each_cons(2) do |a, b|
-          diff = (a - b).abs
-          unless diff.between?(1, 3)
-            safe = false
-            break
-          end
-        end
-      else
-        safe = false
-      end
+      safe = check_safety(data)
 
       @safe_reports_count +=1 if safe
     end
 
     puts "Total count of safe reports is #{@safe_reports_count}"
   end
+
+  def part_two
+    @safe_reports_count = 0
+
+    @input.each do |line|
+        data = line.split.map(&:to_i)
+        safe = check_safety(data)
+
+        unless safe
+          safe = check_with_dampener(data)
+        end
+      @safe_reports_count +=1 if safe
+    end
+
+    puts "Total count of safe reports after problem dampener is #{@safe_reports_count}"
+  end
+
+  private
+
+  def check_safety(data)
+    increasing = data.each_cons(2).all? { |a, b| a < b }
+    decreasing = data.each_cons(2).all? { |a, b| a > b }
+
+    if increasing || decreasing
+      data.each_cons(2) do |a, b|
+        diff = (a - b).abs
+        return false unless diff.between?(1, 3)
+      end
+      true
+    else
+      false
+    end
+  end
+
+  def check_with_dampener(data)
+    data.each_with_index do |_, index|
+      temp_data = remove_element_by_index(data, index)
+      return true if check_safety(temp_data)
+    end
+    false
+  end
+
+  def remove_element_by_index(array, index)
+    array.dup.tap { |copy| copy.delete_at(index) }
+  end
 end
 
 DayTwo.new.part_one
+DayTwo.new.part_two
